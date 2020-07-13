0fc9cf20d7958367132d1c122a4fa1923a2b8a92
nkrokhmal <krokhmal11@mail.ru>
2020-07-13 17:46:17 +0300

    Fix

--- app/stt_client.py
+++ app/stt_client.py
@@ -53,6 +53,7 @@ class SpeechToTextClient:
                 for phrase in recognition_result:
                     for word in phrase:
                         word['word'] = word['word'].replace("'", ' ')
+                print(recognition_result)
                 recognition_result = self.process_sttresult(recognition_result)
                 self.psql_client.update_stt_result(result=json.dumps(recognition_result), dialogue_id=dialogue_id)
                 print('Deleting local path {}'.format(local_file_path))
@@ -72,12 +73,13 @@ class SpeechToTextClient:
 
     def process_sttresult(self, stt_result):
         result = []
-        for word in stt_result:
-            word_format = {}
-            word_format['Word'] = word['word']
-            word_format['Time'] = word['start']
-            word_format['Duration'] = word['end'] - word['start']
-            result.append(word)
+        for phrase in stt_result:
+            for word in phrase:
+                word_format = {}
+                word_format['Word'] = word['word']
+                word_format['Time'] = word['start']
+                word_format['Duration'] = word['end'] - word['start']
+                result.append(word_format)
         return result
 
     def init_app(self, config):
